2207d5ba8d479174242d55b8c8f5076f7079d546
Boban Kilvin <btrajcevski10@gmail.com>
2023-06-27 04:51:17 -0400

    Add more details to current weather display

--- src/weatherui.js
+++ src/weatherui.js
@@ -17,23 +17,39 @@ const WeatherUI = (() => {
   };
 
   const loadCurrent = (currWeather, metric = true) => {
-    const dateTime = document.querySelector(".date-time");
-    const temp = document.querySelector(".temp");
-    const conditionText = document.querySelector(".condition-text");
-    const conditionIcon = document.querySelector(".condition-icon");
+    const weatherWrapper = document.querySelector(".current-weather-wrapper");
+    const dateTime = weatherWrapper.querySelector(".date-time");
+    const tempCurrent = weatherWrapper.querySelector(".temp-current");
+    const conditionText = weatherWrapper.querySelector(".condition-text");
+    const conditionIcon = weatherWrapper.querySelector(".condition-icon");
+    const feelsLike = weatherWrapper.querySelector(".feelslike");
+    const humidity = weatherWrapper.querySelector(".humidity");
+    const wind = weatherWrapper.querySelector(".wind");
 
     dateTime.textContent = currWeather.last_updated;
-    temp.textContent = metric ? currWeather.temp_c : currWeather.temp_f;
-    conditionText.textContent = currWeather.condition.text;
+    tempCurrent.textContent = metric
+      ? `${currWeather.temp_c}°C`
+      : `${currWeather.temp_f}°F`;
 
+    conditionText.textContent = currWeather.condition.text;
     const icon = generateConditionIcon(
       currWeather.condition.code,
       currWeather.is_day
     );
     conditionIcon.append(icon);
+
+    feelsLike.textContent = `Feels like ${
+      metric ? currWeather.feelslike_c : currWeather.feelslike_f
+    } °${metric ? "C" : "F"}`;
+    humidity.textContent = currWeather.humidity + "%";
+    wind.textContent =
+      (metric ? `${currWeather.wind_kph}kph ` : `${currWeather.wind_mph}mph `) +
+      currWeather.wind_dir;
   };
 
-  return { loadCurrent };
+  return {
+    loadCurrent,
+  };
 })();
 
 export { WeatherUI as default };
