f059b3fec5fb9b7aac2a055a8531273280b95008
Boban Kilvin <btrajcevski10@gmail.com>
2023-07-03 01:30:18 -0400

    Add util functions

---
+++ src/utils.js
@@ -0,0 +1,22 @@
+/**
+ * Create an HTML element with any attribute
+ *
+ * @param {string} tagName - Name of HTML tag
+ * @param {{name: string, value: string}} attributes - Object of attributes to be appened to HTML element
+ * @returns {Element} HTML Element
+ */
+function createElement(tagName, attributes) {
+  const element = document.createElement(tagName);
+
+  if (attributes) {
+    Object.entries(attributes).map(([name, value]) => {
+      name === "textContent"
+        ? (element.textContent = value)
+        : element.setAttribute(name, value);
+    });
+  }
+
+  return element;
+}
+
+export { createElement };
