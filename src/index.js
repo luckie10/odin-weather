eefba2c10f01fe8b04bf3f43682fd3afd0c041b2
Boban Kilvin <btrajcevski10@gmail.com>
2023-07-11 03:44:57 -0400

    Finalize index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import WeatherAPI from "./weatherapi";
+import LocationSearch from "./search.js";
+import WeatherUI from "./weatherui";
+
+import "./style.scss";
+
+LocationSearch.attachEventHandlers();
+
+const weather = await WeatherAPI.getForecast(
+  "Windsor, Ontario, Canada",
+  "no",
+  14
+);
+WeatherUI.loadWeather(weather);
